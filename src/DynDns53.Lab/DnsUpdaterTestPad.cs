954a5905d896e446b01f179a73f325284faa290f
volkanp <vpaksoy@rcpsych.ac.uk>
2015-08-10 14:03:17 +0100

    Added lab project

---
+++ src/DynDns53.Lab/DnsUpdaterTestPad.cs
@@ -0,0 +1,63 @@
+﻿using Amazon.Route53;
+using Amazon.Route53.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynDns53.Core
+{
+    public class DnsUpdaterTestPad
+    {
+        private IConfigHandler _configHandler;
+        private IIpChecker _ipChecker;
+        private IAmazonRoute53 _amazonClient;
+
+        public DnsUpdaterTestPad(IConfigHandler configHandler, 
+            IIpChecker ipchecker,
+            IAmazonRoute53 amazonClient)
+        {
+            _configHandler = configHandler;
+            _ipChecker = ipchecker;
+            _amazonClient = amazonClient;
+        }
+
+        public async Task Update()
+        {
+            DynDns53Config config = _configHandler.GetConfig();
+            Task<string> currentExternalIpTask = _ipChecker.GetExternalIpAsync();
+
+            foreach (var domain in config.DomainList)
+            {
+                string subdomain = domain.DomainName;
+                string zoneId = domain.ZoneId;
+
+                ListResourceRecordSetsResponse listResourceRecordSetsResponse = _amazonClient.ListResourceRecordSets(new ListResourceRecordSetsRequest() { HostedZoneId = zoneId });
+                ResourceRecordSet resourceRecordSet = listResourceRecordSetsResponse.ResourceRecordSets.First(recordset => recordset.Name == subdomain);
+                ResourceRecord resourceRecord = resourceRecordSet.ResourceRecords.First();
+
+                string currentExternalIp = await currentExternalIpTask;
+                if (resourceRecord.Value != currentExternalIp)
+                {
+                    resourceRecord.Value = currentExternalIp;
+
+                    _amazonClient.ChangeResourceRecordSets(new ChangeResourceRecordSetsRequest()
+                    {
+                        HostedZoneId = zoneId,
+                        ChangeBatch = new ChangeBatch()
+                        {
+                            Changes = new List<Change>() 
+                            { 
+                                new Change(ChangeAction.UPSERT, resourceRecordSet)
+                            }
+                        }
+                    });
+                }
+                
+            }
+
+        }
+
+    }
+}
---
+++ src/DynDns53.Lab/MethodHost.cs
@@ -0,0 +1,27 @@
+﻿using Amazon;
+using Amazon.Route53;
+using Amazon.Route53.Model;
+using DynDns53.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynDns53.Lab
+{
+    public class MethodHost
+    {
+        public async Task<long> AsyncMethod()
+        {
+            IConfigHandler _configHandler = new AppConfigHandler();
+            var config = _configHandler.GetConfig();
+
+            IAmazonRoute53 _amazonClient = new AmazonRoute53Client(config.Route53AccessKey, config.Route53SecretKey, RegionEndpoint.EUWest1);
+            var request = new GetHealthCheckCountRequest() { };
+            var response = await _amazonClient.GetHealthCheckCountAsync(request);
+
+            return response.HealthCheckCount;
+        }
+    }
+}
